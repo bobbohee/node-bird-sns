2e0747413f20c25957146134d71101e62fd1f59b
park-bohee <parkbohee0705@gmail.com>
2021-01-24 12:37:00 +0900

    [ADD] 프로젝트 기본 세팅 및 Next 라우터 체계 사용

---
+++ front/pages/index.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+const Home = () => {
+    return (
+        <>
+            <Link href="/about">
+                <a>about</a>
+            </Link>
+            <div>Hello, Next!</div>
+        </>
+    );
+};
+
+export default Home;
---
+++ front/pages/user/create.js
@@ -0,0 +1,8 @@
+
+const Create = () => {
+    return (
+        <div>유저를 만들어봅시다.</div>
+    );
+};
+
+export default Create;
